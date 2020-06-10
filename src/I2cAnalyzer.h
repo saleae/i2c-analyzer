310ceee6b92769cde7b50b83dca1a98fc603c928
Marcus10110 <markgarrison@saleae.com>
2020-06-10 14:13:33 -0700

    clang-format update of I2C Analyzer

--- src/I2cAnalyzer.h
+++ src/I2cAnalyzer.h
@@ -8,49 +8,50 @@
 class I2cAnalyzerSettings;
 class I2cAnalyzer : public Analyzer2
 {
-public:
-	I2cAnalyzer();
-	virtual ~I2cAnalyzer();
-	virtual void SetupResults();
-	virtual void WorkerThread();
+  public:
+    I2cAnalyzer();
+    virtual ~I2cAnalyzer();
+    virtual void SetupResults();
+    virtual void WorkerThread();
 
-	virtual U32 GenerateSimulationData( U64 newest_sample_requested, U32 sample_rate, SimulationChannelDescriptor** simulation_channels );
-	virtual U32 GetMinimumSampleRateHz();
+    virtual U32 GenerateSimulationData( U64 newest_sample_requested, U32 sample_rate, SimulationChannelDescriptor** simulation_channels );
+    virtual U32 GetMinimumSampleRateHz();
 
-	virtual const char* GetAnalyzerName() const;
-	virtual bool NeedsRerun();
+    virtual const char* GetAnalyzerName() const;
+    virtual bool NeedsRerun();
 
 
 #pragma warning( push )
-#pragma warning( disable : 4251 ) //warning C4251: 'SerialAnalyzer::<...>' : class <...> needs to have dll-interface to be used by clients of class
+#pragma warning(                                                                                                                           \
+    disable : 4251 ) // warning C4251: 'SerialAnalyzer::<...>' : class <...> needs to have dll-interface to be used by clients of class
 
-protected: //functions
-	void AdvanceToStartBit();
-	void GetByte();
-	bool GetBit( BitState& bit_state, U64& sck_rising_edge );
-	bool GetBitPartOne( BitState& bit_state, U64& sck_rising_edge, U64& frame_end_sample );
-	bool GetBitPartTwo();
-	void RecordStartStopBit();
+  protected: // functions
+    void AdvanceToStartBit();
+    void GetByte();
+    bool GetBit( BitState& bit_state, U64& sck_rising_edge );
+    bool GetBitPartOne( BitState& bit_state, U64& sck_rising_edge, U64& frame_end_sample );
+    bool GetBitPartTwo();
+    void RecordStartStopBit();
 
-protected: //vars
-	std::auto_ptr< I2cAnalyzerSettings > mSettings;
-	std::auto_ptr< I2cAnalyzerResults > mResults;
-	AnalyzerChannelData* mSda;
-	AnalyzerChannelData* mScl;
+  protected: // vars
+    std::auto_ptr<I2cAnalyzerSettings> mSettings;
+    std::auto_ptr<I2cAnalyzerResults> mResults;
+    AnalyzerChannelData* mSda;
+    AnalyzerChannelData* mScl;
 
-	I2cSimulationDataGenerator mSimulationDataGenerator;
-	bool mSimulationInitilized;
+    I2cSimulationDataGenerator mSimulationDataGenerator;
+    bool mSimulationInitilized;
 
-	//Serial analysis vars:
-	U32 mSampleRateHz;
-	bool mNeedAddress;
-	std::vector<U64> mArrowLocations;
+    // Serial analysis vars:
+    U32 mSampleRateHz;
+    bool mNeedAddress;
+    std::vector<U64> mArrowLocations;
 
 #pragma warning( pop )
 };
 
 extern "C" ANALYZER_EXPORT const char* __cdecl GetAnalyzerName();
-extern "C" ANALYZER_EXPORT Analyzer* __cdecl CreateAnalyzer( );
+extern "C" ANALYZER_EXPORT Analyzer* __cdecl CreateAnalyzer();
 extern "C" ANALYZER_EXPORT void __cdecl DestroyAnalyzer( Analyzer* analyzer );
 
-#endif //SERIAL_ANALYZER_H
+#endif // SERIAL_ANALYZER_H
